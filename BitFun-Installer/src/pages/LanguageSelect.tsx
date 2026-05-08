3bc3085ed46f23f85ecd5ee8a1874c3fb476ecb5
kev1n77 <fmy077@outlook.com>
2026-05-08 20:43:00 +0800

    chore: bump test version to v0.2.6-test.1

--- BitFun-Installer/src/pages/LanguageSelect.tsx
+++ BitFun-Installer/src/pages/LanguageSelect.tsx
@@ -72,7 +72,7 @@ export function LanguageSelect({ onSelect }: LanguageSelectProps) {
             opacity: 0.6,
             letterSpacing: '0.5px',
           }}>
-            Version 0.2.5
+            Version 0.2.6-test.1
           </div>
         </div>
       </div>
