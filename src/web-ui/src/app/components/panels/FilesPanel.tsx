8bb62e37e59d6bc552dfb39bf7ecf6cf6d689202
zhangzheng <826193720@qq.com>
2026-07-30 09:43:07 +0800

    fix file rename

--- src/web-ui/src/app/components/panels/FilesPanel.tsx
+++ src/web-ui/src/app/components/panels/FilesPanel.tsx
@@ -47,6 +47,7 @@ import {
   type TransferProgressState,
 } from '@/tools/file-system/services/workspaceFileTransfer';
 import { useWorkspaceFileDrop } from '@/tools/file-system/hooks/useWorkspaceFileDrop';
+import { validateFileName } from '@/tools/file-system/utils/validateFileName';
 import { useShortcut } from '@/infrastructure/hooks/useShortcut';
 import { sshApi } from '@/features/ssh-remote/sshApi';
 import { formatBytes } from '@/shared/utils/format';
@@ -1245,6 +1246,7 @@ const FilesPanel: React.FC<FilesPanelProps> = ({
               onNewFolder={handleNewFolder}
               onRefresh={() => loadFileTree(workspacePath || '', false)}
               hideToolbar={hideExplorerToolbar}
+              isRemoteWorkspace={isRemoteCurrentWorkspace}
             />
           )
         )}
@@ -1328,15 +1330,8 @@ const FilesPanel: React.FC<FilesPanelProps> = ({
         confirmText={inputDialog.type === 'newFile' ? t('dialog.newFile.confirm') : t('dialog.newFolder.confirm')}
         cancelText={inputDialog.type === 'newFile' ? t('dialog.newFile.cancel') : t('dialog.newFolder.cancel')}
         validator={(value) => {
-          const validPattern = isRemoteCurrentWorkspace
-            // eslint-disable-next-line no-control-regex -- filename rules explicitly forbid ASCII control characters.
-            ? /^[^/\x00-\x1F]+$/
-            // eslint-disable-next-line no-control-regex -- filename rules explicitly forbid ASCII control characters.
-            : /^[^<>:"/\\|?*\x00-\x1F]+$/;
-          if (!validPattern.test(value)) {
-            return t('validation.invalidFilename');
-          }
-          return null;
+          const errorKey = validateFileName(value, { isRemote: isRemoteCurrentWorkspace });
+          return errorKey ? t(errorKey) : null;
         }}
       />
     </div>
